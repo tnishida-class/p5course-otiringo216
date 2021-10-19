caa6ae0e5a55c644ff383da4faba1168a964b6d0
otiringo216 <1961073h@stu.kobe-u.ac.jp>
2021-10-19 11:11:50 +0900

    section2-q1

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,17 @@
 // 小手調べ
 function setup(){
+  createCanvas(500,500);
   for(let i = 0; i < 10; i++){
-    // BLANK[1]
+
+  if(i<5){
+  noFill();
+  stroke(0,0,255);
+  }
+  else {
+    noFill();
+    stroke(255,0,0);
+  }
+  let x = i * 10 + 10;
+  ellipse(50, 50, x);  // BLANK[1]
   }
 }
