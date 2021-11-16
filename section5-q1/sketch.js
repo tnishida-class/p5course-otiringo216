cac4aeedd39cd360f1237170a8d8160069410f91
otiringo216 <1961073h@stu.kobe-u.ac.jp>
2021-11-16 12:38:47 +0900

    1961073h

--- section5-q1/sketch.js
+++ section5-q1/sketch.js
@@ -3,16 +3,20 @@
 // 吹き出しにしっぽを付けてみよう
 function setup(){
   createCanvas(400, 400);
+}
+function draw(){
   background(255);
-  balloon("I love keyakizaka46");
+  balloon("I love keyakizaka46",mouseX,mouseY);
 }
 
-function balloon(t){
+function balloon(t,x,y,){
   let w = textWidth(t);
   let h = textAscent() + textDescent();
   let p = 2;
-  fill(0);
-  rect(0, 0, w + p * 2, h + p * 2);
-  fill(255);
-  text(t, p, h + p);
+  fill(188,55,0);
+  rect(x, y, w + p * 2, h + p * 2);
+  triangle(x,y+h,x,y+h+10,x+10,y+h)
+  noStroke();
+  fill(155,299,255);
+  text(t, x+p, y+h + p);
 }
